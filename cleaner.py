820e030cbb163b23c4a50c6b182bcb210b9d1b6b
zandrea418@gmail.com <zandrea418@gmail.com>
2019-08-20 14:02:44 +0800

    feat:数据清洗脚本

--- cleaner.py
+++ cleaner.py
@@ -1,31 +1,68 @@
 # -*- coding: UTF-8 -*-
 import pandas as pd
 import xlrd
+import re
+
+fileName = '编码20150228最终版(查).xlsx'
+df = pd.read_excel(fileName)
 
 class Cleaner:
-    # def __init__(self):
-    #     self.sql_engine = create_engine("mysql+mysqlconnector://root:@127.0.0.1:3306/drug?ssl_disabled='True", echo=False)
-    #     self.conn = mysql.connector.connect(user='root', password='mysql123', database='drug', use_unicode=True, auth_plugin='mysql_native_password')
-    #     self.cursor = self.conn.cursor()
+    def clean(self):
+        spec = '规格型号'
 
+        for idx,row in df.iterrows():
+            cell = df.loc[idx,spec] #str object
+            #去除首尾空格
+            df.set_value(idx,spec,cell.strip())
+            #去除中间空格
+            if ' ' in cell:
+                df.set_value(idx,spec,''.join(cell.split()))
+            # 小写字母装换
+            if (re.search(r'.*[a-z].*',cell) != None):
+                #去除包含单位
+                if(re.match(r'^((?!kW).)*$', cell) != None) and (re.match(r'^((?!kΩ).)*$', cell) != None):
+                        df.set_value(idx,spec,cell.upper())
+            # 替换包含错误下划线模式
+            if (re.search(r'[_]',cell) != None):
+                df.set_value(idx,spec,cell.replace('_', '-'))
+            if (re.search(r'[－]',cell) != None):
+                df.set_value(idx,spec,cell.replace('－', '-'))
 
-    def __init__(self):
-        self.file = '编码20150228最终版(查).xlsx'
+        #检查包含全角数字字母dash
+        for idx,row in df.iterrows():
+            cell = df.loc[idx,spec] #str object
+            if (re.search(r'[^0-9a-zA-Z-]',cell) != None):
+                print(cell)
+                mis = cell
 
-    def readXls(self):
-        df = pd.read_excel(self.file)
-        np_df = df.as_matrix()
-        print(np_df)
-        print(unicode(np_df, encoding="utf-8"))
+        print(' ' in mis.strip())
 
-    # def saveDrug(self):
-    #     df = yzc.getItems()
-    #     np_df = df.as_matrix()
-    #     print(np_df)
-    #     for row in np_df:
-    #         self.cursor.execute('insert into drugInfo(drugSubgroup, drugName, indication) values(%s,%s,%s)',
-    #                             [row[0], row[1], row[2]])
-    #     self.conn.commit()
+        print(df)
+
+        #写入excel
+        df.to_excel('excel_output.xlsx')
+
+        #regex测试
+        eg = " 2000W"
+        # eg = " SCREEN"
+        #去除首尾空格
+        # if ' ' in eg:
+        #     eg = eg.strip()
+        # print(eg)
+
+        #去除中间空格
+        # eg = ''.join(eg.split())
+        # print(eg)
+        # print(re.match(r'[0-9]*', eg) == True)
+
+        #包含小写字母
+        # print(re.match(r'.*[a-z].*',eg))
+
+        #包含特定字符
+        # print(re.match(r'^((?!kW).)*$',eg))
+
+        #检查包含半角数组符号
+        # print(re.search(r'[^0-9a-zA-Z]+',eg))
 
 c = Cleaner()
-c.readXls()
+c.clean()
